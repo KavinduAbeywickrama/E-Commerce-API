2e10ac0a08dd10d80b973310b2832fdc21c93783
KavinduAbeywickrama <madhura-se19054@stu.kln.ac.lk>
2023-09-01 00:55:54 +0530

    updated to the product to retrive count and delete

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,11 @@ const { Category } = require("../models/category");
 const Product = require("../models/product");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find().select('name image -_id');
+    const productList = await Product.find().select('name image');
   
     if (!productList) {
       res.status(500).json({ success: false });
@@ -47,8 +48,68 @@ router.get(`/`, async (req, res) => {
     return res.status(500).send('The product cannot be created!')
     
     res.send(product);
-  
     
 });
+
+router.put('/:id', async(req, res)=> {
+  if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(400).send('Invalid Product Id')
+  }
+
+  const category =await Category.findById(req.body.category);
+  if(!category) return res.status(400).send('Invalid Category!')
   
+  const product = await Product.findByIdAndUpdate(
+
+      req.params.id,
+      {
+          name: req.body.name,
+          description: req.body.description,
+          richDescription: req.body.richDescription,
+          image: req.body.image,
+          brand: req.body.brand,
+          price: req.body.price,
+          category: req.body.category,
+          countInStock: req.body.countInStock,
+          rating: req.body.rating,
+          numReviews: req.body.numReviews,
+          isFeatured: req.body.isFeatured,
+      },
+      {new: true}
+  )
+
+
+  if(!product) {
+      return res.status(400).send('the product cannot be created!')
+      
+      res.send(product);
+
+  }
+})
+
+router.delete('/:id', (req, res)=>{
+  Product.findByIdAndRemove(req.params.id).then(product =>{
+      if(product) {
+          return res.status(200).json({success: true, message: 'the product is deleted!'})
+      } else {
+          return res.status(404).json({success: false, message: 'product not found!'})
+      }
+  }).catch(err=>{
+      return res.status(500).json({success: false, error: err})
+  })
+
+})
+
+router.get(`/get/count`, async (req, res) => {
+  const productCount = await Product.countDocuments()
+
+  if (!productCount) {
+    res.status(500).json({ success: false });
+  } 
+  res.send({
+    productCount: productCount
+  });
+});
+
+
   module.exports = router;
