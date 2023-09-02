c71620e5aac63e06446f501484fff72fe9a4e920
KavinduAbeywickrama <madhura-se19054@stu.kln.ac.lk>
2023-09-02 15:49:15 +0530

    feat: total price added to the order

--- routes/orders.js
+++ routes/orders.js
@@ -41,6 +41,14 @@ router.post('/', async (req, res) => {
 
     const orderItemsIdsResolved = await orderItemsIds;
 
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice;
+    }))
+
+    const totalPrice = totalPrices.reduce((a,b) => a + b , 0);
+    console.log(totalPrice);
     let order = new Order({
         orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -50,7 +58,7 @@ router.post('/', async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     })
     order = await order.save(); 
@@ -87,8 +95,5 @@ router.delete('/:id', (req, res)=>{
         return res.status(400).json({success: false, error: err})
     })
 })
-
-
-
 
 module.exports = router;
