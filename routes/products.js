49e25389ed8c4208b064d0ad7fe14a490c89ba5a
KavinduAbeywickrama <madhura-se19054@stu.kln.ac.lk>
2023-09-03 16:19:18 +0530

    validate image request

--- routes/products.js
+++ routes/products.js
@@ -57,6 +57,10 @@ router.get(`/`, async (req, res) => {
   router.post(`/`, uploadOptions.single('image'), async (req, res) => {
     const category = await Category.findById(req.body.category);
     if (!category) return res.status(400).send("Invalid Category");
+
+    const file = req.file;
+    if (!file) return res.status(400).send("No image in the request");
+
     const filename = req.file.filename;
     const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
 
